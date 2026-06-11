5b58658a30e5d1b685822273fa3e177a6ac0a8b4
lunky12 <lunkyumy@anda.com>
2026-06-11 09:38:37 +0700

    void search

--- searchTree.cpp
+++ searchTree.cpp
@@ -56,3 +56,21 @@ public:
             parent->rightchild = newnode;
         }
     }
+
+    // this function searches the current node of the specified node
+    // as well as the current node of its parent
+    void search(string element, node *&parent, node *&currentnode)
+    {
+        currentnode = root;
+        parent = NULL;
+
+        while (currentnode != NULL && currentnode->info != element)
+        {
+            parent = currentnode;
+
+            if (element < currentnode->info)
+                currentnode = currentnode->leftchild;
+            else
+                currentnode = currentnode->rightchild;
+        }
+    }
