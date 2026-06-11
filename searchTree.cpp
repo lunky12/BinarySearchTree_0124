123a5e16205e0d563eb11d75583de9bb272026c5
lunky12 <lunkyumy@anda.com>
2026-06-11 09:32:46 +0700

    constructor untuk node class

--- searchTree.cpp
+++ searchTree.cpp
@@ -9,4 +9,11 @@ public:
     node *leftchild;
     node *rightchild;
 
-    
+    //constructor for node class
+    node(string i, node *l, node *r)
+    {
+        info = i;
+        leftchild = l;
+        rightchild = r;
+    }
+};
